107e56cad34cd6cf7a4dac7af1f6bdbdb985e8c1
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-26 18:10:16 -0400

    add env vars to paypal

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -5,30 +5,32 @@ export default class MyApp extends React.Component {
   render() {
     const onSuccess = payment => {
       // Congratulation, it came here means everything's fine!
-      console.log("The payment was succeeded!", payment);
+      // console.log("The payment was succeeded!", payment);
+      this.props.clearCart();
+      this.props.history.push("/");
       // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
     const onCancel = data => {
       // User pressed "cancel" or close Paypal's popup!
-      console.log("The payment was cancelled!", data);
+      // console.log("The payment was cancelled!", data);
       // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
     const onError = err => {
       // The main Paypal's script cannot be loaded or somethings block the loading of that script!
-      console.log("Error!", err);
+      // console.log("Error!", err);
       // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
       // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
     };
 
     let env = "sandbox"; // you can set here to 'production' for production
     let currency = "USD"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    // let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: "YOUR-SANDBOX-APP-ID",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
@@ -43,7 +45,7 @@ export default class MyApp extends React.Component {
         env={env}
         client={client}
         currency={currency}
-        total={total}
+        total={this.props.total}
         onError={onError}
         onSuccess={onSuccess}
         onCancel={onCancel}
